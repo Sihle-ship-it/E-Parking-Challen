f877bef671d94bc28f2ce2c602a4ff0ace4904f1
Sihle Mthembu <innocentsihle29@gmail.com>
2022-05-15 17:53:29 +0200

    Added login authentication

--- E-Parking/src/main/java/com/eparking/dao/LoginDAOImpl.java
+++ E-Parking/src/main/java/com/eparking/dao/LoginDAOImpl.java
@@ -1,6 +1,11 @@
 package com.eparking.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.eparking.entity.Login;
+import com.eparking.util.DBConnectionUtil;
 
 public class LoginDAOImpl implements LoginDAO {
 
@@ -8,11 +13,22 @@ public class LoginDAOImpl implements LoginDAO {
 	public String authenticate(Login login) {
 		String sql = "select * from tbl_login_where email=? and password=?";
 		try {
+			Connection connection = DBConnectionUtil.openConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, login.getEmail());
+			ps.setString(2, login.getPassowrd());
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return "true";
+			}else {
+				return "false";
+			}
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		return null;
+		return "error";
 	}
 
 }
